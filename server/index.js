70ba19b21bb8c7157a5dc45fe4f992cabfc2aacd
Bartosz Mogilan <bmogilan@gmail.com>
2025-03-06 16:01:43 +0000

    updating cors

--- server/index.js
+++ server/index.js
@@ -7,8 +7,13 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
 app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 
 // Routes
 const teamsRouter = require('./routes/teams');
