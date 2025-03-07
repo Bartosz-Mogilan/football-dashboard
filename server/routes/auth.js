93adf7728d941a752015a76da05baf26edcaf53d
Bartosz Mogilan <bmogilan@gmail.com>
2025-03-07 13:07:41 +0000

    fixing error

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -20,7 +20,7 @@ router.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const result = await pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username, created_at', [username, hashedPassword]);
-    res.status(500).json({ message: 'User registered successfully', user: result.rows[0]});
+    res.status(201).json({ message: 'User registered successfully', user: result.rows[0]});
   } catch (error) {
     console.error('Error registering user:', error);
     res.status(500).json({ error: 'Database error'});
